7056e5f34b5c88dc6359a11e10071c4d3eecb436
Alonzo Thomas <lenell16@gmail.com>
2015-06-02 18:09:12 -0400

    Add wavy button

--- public/main.js
+++ public/main.js
@@ -2,54 +2,71 @@ var podcasts = {};
 
 var main = function () {
 
-  // Contacting Server
+  // Links to Server
   var rss_link = '/rss?link=';
   var search_link = '/search?term=';
 
-
-  var currentCast = '';
+  // Currently Selected Podcast
+  var currentCast = {};
   var retrievedObj;
   if (retrievedObj = localStorage.getItem('podcasts')) {
     podcasts = JSON.parse(retrievedObj);
   }
 
-  var table = $('table');
+  var episodeTable = $('table');
   var castList = $('.cast-list');
-  var playPause = $('.playPause');
+  var playPauseButton = $('.playPause');
+  var player = $('#podPlayer');
+
+
+  player.bind('timeupdate', function () {
+    var currentTime = Math.floor(this.currentTime);
+    $('#seeker').val(currentTime);
+    function zeroFill( number, width )
+    {
+      width -= number.toString().length;
+      if ( width > 0 )
+      {
+        return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+      }
+      return number + ""; // always return a string
+    }
+    var momentDuration = moment.duration(currentTime, 'seconds');
+    var hrs = zeroFill(momentDuration.hours(), 2);
+    var min = zeroFill(momentDuration.minutes(), 2);
+    var sec = zeroFill(momentDuration.seconds(), 2);
+    $('#currTime').text(hrs +':'+min+':' +sec );
+  });
+
+  function castClick() {
+    var castTitle = $(this).attr('data-cast');
+    currentCast = podcasts[castTitle];
+    displayEpisodes();
+  }
+
+  function addCast(podcast) {
+    castList.append($('<div>', {
+      'class': 'col s4',
+      'data-cast': podcast.meta.title,
+      click: castClick
+    }).append($('<img>', {
+      src: podcast.meta.image.url
+    })));
+  }
 
   function loadCasts() {
     for (podcastTitle in podcasts) {
-      castList.append($('<div>', {
-        'class': 'col s4',
-        'data-cast': podcastTitle,
-        click: function () {
-          console.log($(this).attr('data-cast'));
-          displayEpisodes($(this).attr('data-cast'));
-          currentCast = $(this).attr('data-cast');
-        }
-      }).append($('<img>', {
-        src: podcasts[podcastTitle].meta.image.url
-      })));
+      addCast(podcasts[podcastTitle]);
     }
   }
 
   loadCasts();
 
   function getFeed(feed_url) {
-    $.get(rss_link + feed_url, function (data) {
-      podcasts[data.meta.title] = data;
+    $.get(rss_link + feed_url, function (podcast) {
+      podcasts[podcast.meta.title] = data;
       localStorage.setItem('podcasts', JSON.stringify(podcasts));
-      castList.append($('<div>', {
-        'class': 'col s4',
-        'data-cast': data.meta.title,
-        click: function () {
-          displayEpisodes($(this).attr('data-cast'));
-          currentCast = $(this).attr('data-cast');
-        }
-      }).append($('<img>', {
-        src: data.meta.image.url
-      })));
-
+      addCast(podcast);
     }, "json");
   }
 
@@ -62,71 +79,53 @@ var main = function () {
 
   function displayEpisodes(feedTitle) {
     $('tbody').remove();
-    podcasts[feedTitle].feed.forEach(function (episode, index) {
+    for (var i = 0; i < currentCast.feed.length; i++) {
+      var episode = currentCast.feed[i];
       var row = $('<tr>');
       var cell = $('<td>');
 
       cell.text(episode.title);
       if(episode.enclosures.length > 0){
-        cell.attr('data-index', index);
+        cell.attr('data-index', i);
       }else{
         console.log(episode.title);
       }
       cell.addClass('episode');
       row.append(cell);
-      table.append(row);
-    });
+      episodeTable.append(row);
+    }
   }
 
-  $('button').click(function () {
+  $('#addFeed').click(function () {
     var term = $('#searchbox').val();
     getFeed(term);
   });
 
 
   $('body').on('click','td', function() {
-    var player = $('audio');
     var index = $(this).attr('data-index');
-    player.attr('src', podcasts[currentCast].feed[index].enclosures[0].url);
-    var duration = moment.duration(podcasts[currentCast].feed[index]['itunes:duration']['#']).asSeconds();
+    var selectedEpisode = currentCast.feed[index];
+    player.attr('src', selectedEpisode.enclosures[0].url);
+    var episodeDuration = selectedEpisode['itunes:duration']['#'];
+    var duration = moment.duration(episodeDuration).asSeconds();
     $('#seeker').attr('max', duration);
     player.trigger('play');
-    playPause.removeClass('mdi-av-play-arrow');
-    playPause.addClass('mdi-av-pause');
+    playPauseButton.removeClass('mdi-av-play-arrow');
+    playPauseButton.addClass('mdi-av-pause');
     $('#currTime').text('0:00:00');
-    $('#endTime').text(podcasts[currentCast].feed[index]['itunes:duration']['#']);
+    $('#endTime').text(episodeDuration);
   });
 
-  $('body').append($('<audio>', {'id': 'podPlayer'}));
-  $('audio').bind('timeupdate', function () {
-    $('#seeker').val(Math.floor(this.currentTime));
-    function zeroFill( number, width )
-    {
-      width -= number.toString().length;
-      if ( width > 0 )
-      {
-        return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
-      }
-      return number + ""; // always return a string
-    }
-    var hrs = moment.duration(Math.floor(this.currentTime), 'seconds').hours();
-    var min = zeroFill(moment.duration(Math.floor(this.currentTime), 'seconds').minutes(), 2);
-    var sec = zeroFill(moment.duration(Math.floor(this.currentTime), 'seconds').seconds(), 2);
-    console.log(hrs, min, sec);
-    $('#currTime').text('' + hrs +':'+min+':' +sec );
-  });
 
-  playPause.click(function() {
-    var player = $('audio');
-    if (player.get(0).paused == false) {
+
+  playPauseButton.click(function() {
+    if (!player.get(0).paused) {
       player.trigger('pause');
-      playPause.removeClass('mdi-av-pause');
-      playPause.addClass('mdi-av-play-arrow');
     } else {
       player.trigger('play');
-      playPause.removeClass('mdi-av-play-arrow');
-      playPause.addClass('mdi-av-pause');
     }
+    playPauseButton.toggleClass('mdi-av-pause');
+    playPauseButton.toggleClass('mdi-av-play-arrow');
   });
 
 };
